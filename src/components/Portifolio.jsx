43188726ff8fb7ddfcada0a505f0666b16ff6c76
Allanpt <jatallantavares@gmail.com>
2023-10-02 20:52:44 -0300

    img portifolio

--- src/components/Portifolio.jsx
+++ src/components/Portifolio.jsx
@@ -1,7 +1,12 @@
 import './Portifolio.css'
 import { Icon } from '@iconify/react';
 import { useState, useEffect } from 'react';
-
+import LandingPage from '../../public/Landing-page.webp'
+import GifForm from '../../public/gif-form.gif'
+import GifDev from '../../public/gif-devnote.gif'
+import JDBC from '../../public/Jdbc-project.png'
+import SpringJPA from '../../public/spring-boot-jpa.webp'
+import SpringMongo from '../../public/spring-mongo.png'
 const Portifolio = () => {
 
     const [isVisible, setIsVisible] = useState(false);
@@ -29,7 +34,7 @@ const Portifolio = () => {
         <div className="portifolio-line">
             <div className="project one">
                 <a href="https://allanpt.github.io/Mais1Code_Project_Landing_Page/" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio landing-page' src="../../public/Landing-page.JPG" alt="Landing page" />
+                    <img className='img-portifolio landing-page' src={LandingPage} alt="Landing page" />
                     <div className="tecnologies">
                         <div className="tec">HTML</div>
                         <div className="tec">CSS</div>
@@ -41,7 +46,7 @@ const Portifolio = () => {
             </div>
             <div className="project two">
                 <a href="https://allanpt.github.io/DevNotes/" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio devnote' src="/gif-devnote.gif" alt="DevNote" />
+                    <img className='img-portifolio devnote' src={GifDev} alt="DevNote" />
                     <div className="tecnologies">
                         <div className="tec">HTML</div>
                         <div className="tec">CSS</div>
@@ -52,7 +57,7 @@ const Portifolio = () => {
             </div>
             <div className="project three">
                 <a href="https://allanpt.github.io/MultiStepForm_react/" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio form-avaliation' src="/gif-form.gif" alt="Formulário de Avaliação" />
+                    <img className='img-portifolio form-avaliation' src={GifForm} alt="Formulário de Avaliação" />
                     <div className="tecnologies">
                         <div className="tec">React</div>
                     </div>
@@ -61,7 +66,7 @@ const Portifolio = () => {
             </div>
             <div className="project four">
                 <a href="https://github.com/Allanpt/Web_Services_Spring_Boot_JPA_Hibernate" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio jpa' src="/spring-boot-jpa.webp" alt="Web services jpa" />
+                    <img className='img-portifolio jpa' src={SpringJPA} alt="Web services jpa" />
                     <div className="tecnologies">
                         <div className="tec">Spring Boot</div>
                         <div className="tec">PostgreSQL</div>
@@ -72,7 +77,7 @@ const Portifolio = () => {
             </div>
             <div className="project five">
                 <a href="https://github.com/Allanpt/API_Restful_Spring_Boot_MongoDB_Udemy_project" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio mongo' src="/spring-mongo.png" alt="APi Restfull" />
+                    <img className='img-portifolio mongo' src={SpringMongo} alt="APi Restfull" />
                     <div className="tecnologies">
                         <div className="tec">Spring Boot</div>
                         <div className="tec">MongoDB</div>
@@ -82,7 +87,7 @@ const Portifolio = () => {
             </div>
             <div className="project six">
                 <a href="https://github.com/Allanpt/Dao_JDBC_project" target="_blank" rel="noopener noreferrer">
-                    <img className='img-portifolio jdbc' src="/Jdbc-project.png" alt="JDBC porject" />
+                    <img className='img-portifolio jdbc' src={JDBC} alt="JDBC porject" />
                     <div className="tecnologies">
                         <div className="tec">JDBC</div>
                         <div className="tec">MySQL</div>
